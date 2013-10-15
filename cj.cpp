b00c9f48c537144dda149ba2d8b53e3ec2c34b05
Alex Kesling <alex.kesling@gmail.com>
2013-10-14 23:41:32 -0400

    Working parser

--- cj.cpp
+++ cj.cpp
@@ -1,6 +1,47 @@
+#include <stdio.h>
 #include <iostream>
 
+using namespace std;
+
+enum INSTRUCTION {
+    INC = 43, IN, DEC, OUT,
+    LANGLE = 60,
+    RANGLE = 62,
+    LBRACK = 91,
+    RBRACK = 93,
+};
+
 int main() {
-    std::cout << "Hello world!";
+    INSTRUCTION input;;
+    while ((input = static_cast<INSTRUCTION>(getchar_unlocked())) != -1) {
+        switch(input) {
+            case LANGLE:
+                putchar_unlocked(input);
+                break;
+            case RANGLE:
+                putchar_unlocked(input);
+                break;
+            case INC:
+                putchar_unlocked(input);
+                break;
+            case DEC:
+                putchar_unlocked(input);
+                break;
+            case LBRACK:
+                putchar_unlocked(input);
+                break;
+            case RBRACK:
+                putchar_unlocked(input);
+                break;
+            case OUT:
+                putchar_unlocked(input);
+                break;
+            case IN:
+                putchar_unlocked(input);
+                break;
+            default:
+                cout << "Invalid char: " << (int) input << "\n";
+        }
+    }
     return 0;
 }
